ffd93bf14060d3adbd59253d7ab7f1d7c78252bf
Naval15 <e23cseu0962@bennett.edu.in>
2025-06-18 18:29:30 +0530

    done

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,10 +1,5 @@
-export const USER_API_END_POINT="http://44.219.138.169:8000/api/v1/user";
-export const JOB_API_END_POINT="http://44.219.138.169:8000/api/v1/job";
-export const APPLICATION_API_END_POINT="http://44.219.138.169:8000/api/v1/application";
-export const COMPANY_API_END_POINT="http://44.219.138.169:8000/api/v1/company";
-export const SAVED_JOBS_API_END_POINT="http://44.219.138.169:8000/api/savedjobs";
-// export const USER_API_END_POINT="https://www.hirees.com/api/v1/user";
-// export const JOB_API_END_POINT="https://www.hirees.com/api/v1/job";
-// export const APPLICATION_API_END_POINT="https://www.hirees.com/api/v1/application";
-// export const COMPANY_API_END_POINT="https://www.hirees.com/api/v1/company";
-// export const SAVED_JOBS_API_END_POINT="https://www.hirees.com/api/savedjobs";
+export const USER_API_END_POINT="https://www.hirees.com/api/v1/user";
+export const JOB_API_END_POINT="https://www.hirees.com/api/v1/job";
+export const APPLICATION_API_END_POINT="https://www.hirees.com/api/v1/application";
+export const COMPANY_API_END_POINT="https://www.hirees.com/api/v1/company";
+export const SAVED_JOBS_API_END_POINT="https://www.hirees.com/api/savedjobs";
